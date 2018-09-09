22f252e389734d5cb00a97292656be7c68511799
Sunghyun Hwang <me@sunghyunzz.com>
2018-09-09 15:54:24 +0900

    Implement K-Property
    Signed-off-by: Sunghyun Hwang <me@sunghyunzz.com>

---
+++ kformat/kproperty.py
@@ -0,0 +1,93 @@
+import abc
+from datetime import date, time
+from typing import Optional, Set
+
+__all__ = ['AN', 'N']
+
+
+TYPES = Set[type]
+
+
+class KProperty(metaclass=abc.ABCMeta):
+
+    def __init__(
+        self,
+        length: int,
+        expected_types: TYPES,
+        filler: bytes
+    ) -> None:
+        self.length = length
+        self.expected_types = expected_types
+        self.filler = filler
+
+    @abc.abstractmethod
+    def to_bytes(self, v: Optional) -> bytes:
+        pass
+
+
+class N(KProperty):
+
+    EXPECTED_TYPES = {int, float}
+    FILLER = b'0'
+    ENCODING = 'ascii'
+
+    def __init__(
+        self,
+        length: int,
+        *,
+        filler: Optional[bytes]=None
+    ) -> None:
+        super().__init__(
+            length,
+            N.EXPECTED_TYPES,
+            filler or N.FILLER
+        )
+
+    def to_bytes(self, v: Optional) -> bytes:
+        s = str(int(v)) if v is not None else ''
+        try:
+            b = bytes(s, encoding=N.ENCODING).rjust(self.length, self.filler)
+            assert len(b) <= self.length
+            return b
+        except AssertionError:
+            raise ValueError(f'Too long value is given(max: {self.length})')
+
+
+class AN(KProperty):
+
+    EXPECTED_TYPES = N.EXPECTED_TYPES | {str, date, time}
+    FILLER = b' '
+    ENCODING = 'euc-kr'
+    DATE_FORMAT = '%Y%m%d'
+    TIME_FORMAT = '%H%M%S%f'
+    TIME_FORMAT_SLICE = 0, -2
+
+    def __init__(
+        self,
+        length: int,
+        *,
+        filler: Optional[bytes]=None
+    ) -> None:
+        super().__init__(
+            length,
+            AN.EXPECTED_TYPES,
+            filler or AN.FILLER
+        )
+
+    def to_bytes(self, v: Optional) -> bytes:
+        if v is None or isinstance(v, str):
+            s = v if v else ''
+        elif isinstance(v, date):
+            s = v.strftime(AN.DATE_FORMAT)
+        elif isinstance(v, time):
+            s = v.strftime(AN.TIME_FORMAT)[slice(*AN.TIME_FORMAT_SLICE)]
+        else:
+            s = str(int(v))
+
+        try:
+            b = bytes(s, encoding=AN.ENCODING).ljust(self.length, self.filler)
+            assert len(b) <= self.length
+            return b
+        except AssertionError:
+            raise ValueError(f'Too long value is given(max: {self.length})')
+
---
+++ tests/test_kproperty.py
@@ -0,0 +1,60 @@
+import unittest
+from datetime import date, time
+
+from kformat.kproperty import *
+
+
+class TestKProperty(unittest.TestCase):
+
+    def test_N_to_bytes(self):
+        self.assertEqual(
+            N(5).to_bytes(None),
+            b'00000'
+        )
+        self.assertEqual(
+            N(5).to_bytes(0),
+            b'00000'
+        )
+        self.assertEqual(
+            N(5).to_bytes(3),
+            b'00003'
+        )
+        self.assertEqual(
+            N(5).to_bytes(12345),
+            b'12345'
+        )
+        self.assertEqual(
+            N(10, filler=b'?').to_bytes(3),
+            b'?????????3'
+        )
+
+        with self.assertRaises(ValueError):
+            N(3).to_bytes(1234)
+
+    def test_AN_to_bytes(self):
+        self.assertEqual(
+            AN(10).to_bytes('sunghyunzz'),
+            b'sunghyunzz'
+        )
+        self.assertEqual(
+            AN(10).to_bytes('황성현'),
+            b'\xc8\xb2\xbc\xba\xc7\xf6    '
+        )
+        self.assertEqual(
+            AN(10).to_bytes(date(2018, 9, 9)),
+            b'20180909  '
+        )
+        self.assertEqual(
+            AN(10).to_bytes(time(15, 47, 0, 0)),
+            b'1547000000'
+        )
+        self.assertEqual(
+            AN(12).to_bytes(time(15, 35, 12, 345678)),
+            b'1535123456  '
+        )
+        self.assertEqual(
+            AN(5).to_bytes(1),
+            b'1    '
+        )
+        with self.assertRaises(ValueError):
+            AN(5).to_bytes(date(2018, 9, 9))
