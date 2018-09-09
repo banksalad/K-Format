7eb0ca769e282d934c74b42dc9962846b1d4a14b
Sunghyun Hwang <me@sunghyunzz.com>
2018-09-09 15:55:07 +0900

    Configure K-Format package
    Signed-off-by: Sunghyun Hwang <me@sunghyunzz.com>

--- kformat/__init__.py
+++ kformat/__init__.py
@@ -1,42 +1,5 @@
+from .kclass import *
+from .kproperty import *
 
 
-class KProp:
-    length: int
-
-    def __init__(self, length: int) -> None:
-        self.length = length
-
-
-class A(KProp):
-    pass
-
-
-class AN(KProp):
-    pass
-
-
-def _kclass(cls):
-
-    props = [(k, prop) for (k, prop) in cls.__annotations__.items()]
-
-    def init(self, *args):
-        for ((k, prop), v) in zip(props, args):
-            # TODO: type checking
-            setattr(self, k, (prop, v))
-
-    setattr(cls, '__init__', init)
-
-    return cls
-
-
-def kclass(_cls=None):
-    return _kclass(_cls)
-
-
-@kclass
-class Sth:
-    name: A(50)
-    age: AN(3)
-
-
-print(Sth(1, 2).name)
+__all__ = ['kclass', 'N', 'AN']
