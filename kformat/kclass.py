0fe58ed3056fa0aef40244ca33cb7f3f2489bda6
Sunghyun Hwang <me@sunghyunzz.com>
2018-09-09 15:54:33 +0900

    Implement K-Class
    Signed-off-by: Sunghyun Hwang <me@sunghyunzz.com>

---
+++ kformat/kclass.py
@@ -0,0 +1,29 @@
+from . import kproperty
+
+
+__all__ = ['kclass']
+
+
+def _kclass(cls):
+
+    props = [(k, prop) for (k, prop) in cls.__annotations__.items()]
+
+    def init(self, *args):
+        prop_bytes = []
+
+        for ((k, prop), v) in zip(props, args):
+            assert isinstance(prop, kproperty.KProperty)
+            assert type(v) in prop.expected_types
+
+            setattr(self, k, (prop, v))
+            prop_bytes.append(prop.to_bytes(v))
+
+        setattr(cls, 'bytes', b''.join(prop_bytes))
+
+    setattr(cls, '__init__', init)
+
+    return cls
+
+
+def kclass(_cls=None):
+    return _kclass(_cls)
---
+++ tests/test_kclass.py
@@ -0,0 +1,35 @@
+import unittest
+
+from kformat.kclass import kclass
+from kformat.kproperty import AN, N
+
+
+class TestKClass(unittest.TestCase):
+
+    def test_kclass_init(self):
+
+        @kclass
+        class Something:
+            n: N(10)
+            an: AN(20)
+
+        sth = Something(123, "k-class")
+        self.assertIsNotNone(sth)
+
+    def test_kclass_to_bytes(self):
+        _n, N.to_bytes = N.to_bytes, lambda s, v: b'N'
+        _an, AN.to_bytes = AN.to_bytes, lambda s, v: b'AN'
+
+        @kclass
+        class Something:
+            a: N(10)
+            b: AN(20)
+            c: N(5)
+            d: AN(10)
+
+        sth = Something(1, 2, 3, 4)
+        self.assertEqual(sth.bytes, b'NANNAN')
+
+        # Reset to_bytes funcs to default
+        N.to_bytes = _n
+        AN.to_bytes = _an
