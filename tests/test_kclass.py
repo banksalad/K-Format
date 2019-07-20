47376ba15703e632bcb54fe493691e2115d4e44c
jungwinter <wintermy201@gmail.com>
2019-07-20 15:20:38 +0900

    Improve kclass testing about wrong type initiating

--- tests/test_kclass.py
+++ tests/test_kclass.py
@@ -1,6 +1,8 @@
 from unittest.mock import patch
 from typing import List
 
+import pytest
+
 from kformat.kclass import kclass
 from kformat.kproperty import AN, N
 
@@ -51,3 +53,54 @@ def test_list_of_kclass_creation():
 
     some_list = [Something(1), Something(2), Something(3)]
     assert b''.join(s.bytes for s in some_list) == b'123'
+
+
+class TestWrongTypeInit:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        @kclass
+        class Other:
+            an: AN(10)
+            n: N(5)
+
+        @kclass
+        class Something:
+            other: Other
+            others: List[Other]
+
+        self.other = Other
+        self.something = Something
+
+    def test_prop_is_not_kclass(self):
+        with pytest.raises(AssertionError) as e:
+            self.something(1, [1, 2])
+        assert str(e.value) == 'int is not type of Other'
+
+    def test_prop_is_not_list(self):
+        with pytest.raises(AssertionError) as e:
+            self.something(self.other(1, 2), 3)
+        assert str(e.value) == 'int is not List'
+
+    def test_all_items_are_not_kclass(self):
+        with pytest.raises(AssertionError) as e:
+            self.something(self.other(1, 2), [self.other(3, 4), 5])
+        assert str(e.value) == 'All of list items should be type of K-Class'
+
+    def test_prop_is_not_k_property(self):
+        @kclass
+        class One:
+            an: AN(10)
+            n: int
+
+        with pytest.raises(AssertionError) as e:
+            One(1, 2)
+        assert str(e.value) == 'int is not subtype of KProperty'
+
+    def test_prop_is_not_expected_type(self):
+        with pytest.raises(AssertionError) as e:
+            self.other(1, '2')
+        assert str(e.value) == 'N cannot accept str type'
+
+        with pytest.raises(AssertionError) as e:
+            self.other([1], 2)
+        assert str(e.value) == 'AN cannot accept list type'
