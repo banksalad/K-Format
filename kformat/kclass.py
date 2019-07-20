1a820d6b14b2162d8a5cd9cf6fac7e8cffad4efa
jungwinter <wintermy201@gmail.com>
2019-07-20 21:09:29 +0900

    Improve readability

--- kformat/kclass.py
+++ kformat/kclass.py
@@ -1,5 +1,5 @@
-from . import kproperty
 from .exception import UnexpectedTypeError
+from .kproperty import KProperty
 
 __all__ = ['kclass']
 
@@ -15,18 +15,22 @@ def _is_prop_list(prop) -> bool:
     return hasattr(prop, '__origin__') and prop.__origin__ == list
 
 
+def _is_valid_child_prop(prop) -> bool:
+    return (
+        _is_prop_kclass(prop)
+        or _is_prop_list(prop)
+        or isinstance(prop, KProperty)
+    )
+
+
 def _kclass(cls):
     setattr(cls, KCLASS_ANNOTATION, True)
 
     props = list(cls.__annotations__.items())
 
     for _, prop in props:
-        if (
-            not _is_prop_kclass(prop)
-            and not _is_prop_list(prop)
-            and not isinstance(prop, kproperty.KProperty)
-        ):
-            raise UnexpectedTypeError(kproperty.KProperty, prop.__name__)
+        if not _is_valid_child_prop(prop):
+            raise UnexpectedTypeError(KProperty, prop.__name__)
 
     @property
     def to_bytes(self):
@@ -44,17 +48,17 @@ def _kclass(cls):
                 if not isinstance(v, list):
                     raise UnexpectedTypeError(list, type(v))
                 for item in v:
-                    if not getattr(item, KCLASS_ANNOTATION, False):
+                    if not _is_prop_kclass(item):
                         raise UnexpectedTypeError(
                             f'List[{prop.__args__[0].__name__}]', type(item)
                         )
                 prop_bytes.extend(c.bytes for c in v)
             else:
                 if type(v) not in prop.expected_types:
-                    expects = ', '.join(
+                    expected_types = ', '.join(
                         sorted(t.__name__ for t in prop.expected_types)
                     )
-                    raise UnexpectedTypeError(expects, type(v))
+                    raise UnexpectedTypeError(expected_types, type(v))
                 prop_bytes.append(prop.to_bytes(v))
 
             setattr(self, k, (prop, v))
